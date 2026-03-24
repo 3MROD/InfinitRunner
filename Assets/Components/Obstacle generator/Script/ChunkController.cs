f9c6039ac884cf207c81d6c9c80565e2c111da0c
3MROD <jadejogi@hotmail.fr>
2026-03-24 17:54:47 +0100

    trying Chunk generation
    test

--- Assets/Components/Obstacle generator/Script/ChunkController.cs
+++ Assets/Components/Obstacle generator/Script/ChunkController.cs
@@ -3,5 +3,10 @@ using UnityEngine;
 public class ChunkController : MonoBehaviour
 {
     [SerializeField] private Transform _endAnchor;
-   public Vector3 EndAnchor => _endAnchor.position; 
+   public Vector3 EndAnchor => _endAnchor.position;
+
+   public bool IsBehindPlayer()
+   {
+       return EndAnchor.z < 0;
+   }
 }
