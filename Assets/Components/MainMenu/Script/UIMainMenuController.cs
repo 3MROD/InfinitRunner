1d90b871064b11fd528233bbbc933e5e1e99491d
3MROD <jadejogi@hotmail.fr>
2026-04-05 22:24:48 +0200

    Add serialization
    add a simple save service to serialize data on the disk

--- Assets/Components/MainMenu/Script/UIMainMenuController.cs
+++ Assets/Components/MainMenu/Script/UIMainMenuController.cs
@@ -1,21 +1,34 @@
+using Components.SaveService;
+using TMPro;
 using UnityEngine;
 
 public class UIMainMenuController : MonoBehaviour
 {
-    public void QuitGame()
+    [SerializeField] private TMP_Text _runCountText;
+    private SaveData _saveData;
+    
+    private void Start()
     {
-        #if !UNITY_EDITOR
-        Application.Quit();
-        #else
-        UnityEditor.EditorApplication.isPlaying = false;
-        #endif
-
+        var saveData = SaveService.Load();
+        _saveData = saveData ?? new SaveData();
         
-        
+        _runCountText.text = "Runs: " + _saveData.RunCount;
     }
 
-    public void StarGame()
+    public void StartGame()
     {
+        _saveData.RunCount++;
+        SaveService.Save(_saveData);
+        
         SceneLoaderService.LoadGame();
+    }
+    
+    public void QuitGame()
+    {
+#if !UNITY_EDITOR
+        Application.Quit();
+#else
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
